94f8f8123f82c926a7072426807f149ec643db35
gggghale <falquezamiguel@gmail.com>
2020-03-09 12:39:55 +0800

    third commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
-ctr = 0;
-setInterval(function(){
-    ctr +=2;
-    document.write(`${ctr} secs. has elapsed...<br>`);
-}, 2000);
+let promise = new Promise(function(resolve,reject){
+    grade = 75;
+    if(grade >=60){
+        resolve("pass");
+    }
+    else{
+        reject("fail");
+    }
+});
+
+promise.then(function(message){
+    document.write(`You ${message} the subject`);
+
+
+}).catch(function(message){
+    document.write(`You ${message} the subject`)
+});
